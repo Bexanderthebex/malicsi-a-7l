5bc6aec3670a9c20fe95537d878749d1675cc09f
Brixjohn <brlumabi@up.edu.ph>
2017-03-07 04:52:52 +0800

    Added tables for entities game and sport

--- malicsi.sql
+++ malicsi.sql
@@ -32,7 +32,7 @@ CREATE TABLE organizer (
 );
 
 CREATE TABLE sponsor_institution (
-	sponsor_id INT AUTO INCREMENT,
+	sponsor_id INT AUTO_INCREMENT,
 	name VARCHAR(100) NOT NULL,
 	description VARCHAR(200),
 	PRIMARY KEY(sponsor_id)
@@ -47,7 +47,7 @@ CREATE TABLE sponsor_games (
 );
 
 CREATE TABLE match (
-	match_id INT AUTO INCREMENT,
+	match_id INT AUTO_INCREMENT,
 	winner INT NOT NULL,
 	time_start TIME NOT NULL,
 	time_end TIME NOT NULL,
@@ -64,4 +64,36 @@ CREATE TABLE team_in_match (
 	PRIMARY KEY(match_id, team_id),
 	FOREIGN KEY(match_id) references match(match_id),
 	FOREIGN KEY(team_id) references team(team_id)
+);
+
+CREATE TABLE game (
+	game_id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	start_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	location VARCHAR(100) NOT NULL,
+	description VARCHAR(100),
+	overall_winner VARCHAR(50) NOT NULL,
+	PRIMARY KEY (game_id),
+	FOREIGN KEY (overall_winner) REFERENCES organization_in_game(organization)
+);
+
+CREATE TABLE organization_in_game (
+	game_id INT NOT NULL,
+	organization VARCHAR(50) NOT NULL,
+	PRIMARY KEY (game_id, organization),
+	FOREIGN KEY (game_id) REFERENCES game(game_id)
+);
+
+CREATE TABLE sport (
+	sport_id INT NOT NULL AUTO_INCREMENT,
+	winner INT NOT NULL,
+	time_start TIME NOT NULL,
+	time_end TIME NOT NULL,
+	date DATE NOT NULL,
+	scoring_system VARCHAR(50) NOT NULL,
+	game_id INT NOT NULL,
+	PRIMARY KEY (sport_id),
+	FOREIGN KEY (winner) REFERENCES team(team_id),
+	FOREIGN KEY (game_id) REFERENCES game(game_id)
 );
