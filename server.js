1d0523996a86d97219458b868b33337d3c6dc3cd
Manuel Antonio P. Rufino <mprufino@up.edu.ph>
2017-03-10 18:33:30 +0800

    Set up session.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const session = require('express-session');
+const cookieSession = require('cookie-session');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 
@@ -12,6 +12,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // place session
+//sha256 of the string 'malicsi' (minus quotes)
+app.use(cookieSession({secret: '65508d52c495cc86e75eed713f253d9777c27fb2bda2701a50581334b95f4ac2'}));
 
 // place other routing stuff
 app.use(routes);
