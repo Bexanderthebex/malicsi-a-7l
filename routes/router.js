2f7a99ea961d5ed33c53025b6f96447f57973023
Brixjohn <brlumabi@up.edu.ph>
2017-03-14 19:56:48 +0800

    Added add match

--- routes/router.js
+++ routes/router.js
@@ -5,3 +5,8 @@ var router = express.Router();
 
 var profileController = require("../controllers/sample-controller");
 router.get('/getSampleSession', profileController.getSampleSession);
+
+var matchController = require("../controllers/match-controller");
+router.post('/addMatch', matchController.addMatch);
+
+module.exports = router;
