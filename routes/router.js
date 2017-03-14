60b7519e954e39014e1e24d5be782728cc2d23bd
Brixjohn <brlumabi@up.edu.ph>
2017-03-14 20:46:33 +0800

    Edited add match

--- routes/router.js
+++ routes/router.js
@@ -6,6 +6,10 @@ var router = express.Router();
 var profileController = require("../controllers/sample-controller");
 router.get('/getSampleSession', profileController.getSampleSession);
 
+var sportController = require("../controllers/sport-controller");
+router.post('/createSport', sportController.createSport);
+router.get('/sport/:sportID', sportController.viewSportDetails);
+
 var matchController = require("../controllers/match-controller");
 router.post('/addMatch', matchController.addMatch);
 
