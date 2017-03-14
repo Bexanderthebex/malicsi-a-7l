34051cbf458b9ca3d5d729233584746a02309500
Brixjohn <brlumabi@up.edu.ph>
2017-03-14 20:37:16 +0800

    Added view sport details

---
+++ controllers/sport-controller.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const connection = require('./../config/db-connection.js');
+
+exports.createSport = (req, res) => {
+	const newSport = {
+		time_start: req.body.timeStart,
+		time_end: req.body.timeEnd,
+		date: req.body.date,
+		scoring_system: req.body.scoringSystem,
+		game_id: req.body.gameID
+	}
+
+	connection.query('INSERT INTO sport SET ?', newSport, (err, rows, fields) => {
+		if (err) throw err;
+		res.send(results);	
+	})
+}
+
+exports.viewSportDetails = (req, res) => {
+	connection.query('SELECT * FROM sport where sport_id = ?', req.params.sportID, (err, rows, fields) => {
+		if (err) throw err;
+		res.send(results);
+	})
+}
--- routes/router.js
+++ routes/router.js
@@ -5,3 +5,9 @@ var router = express.Router();
 
 var profileController = require("../controllers/sample-controller");
 router.get('/getSampleSession', profileController.getSampleSession);
+
+var sportController = require("../controllers/sport-controller");
+router.post('/createSport', sportController.createSport);
+router.get('/sport/:sportID', sportController.viewSportDetails);
+
+module.exports = router;
