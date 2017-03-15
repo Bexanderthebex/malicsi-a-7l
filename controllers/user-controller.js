8447dc0d94c2db7e3d196d93d3f1896bd3614223
pvtan <pvtan@up.edu.ph>
2017-03-15 08:41:45 +0800

    Added search competitor functionality

---
+++ controllers/user-controller.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const connection = require('./../config/db-connection.js');
+
+exports.searchCompetitor = (req, res) => {
+	const competitor = {
+		search : req.query.search
+	}
+
+	function searchCompetitor() {
+		const queryString = 'SELECT id, birthday, first_name, last_name, nickname, sex FROM competitor'
+		+ 'where first_name like ? '
+		+ 'or last_name like ? '
+		+ 'or nickname like ?';
+
+		const queryParameters = [
+			"%" + competitor.search + "%",
+			"%" + competitor.search + "%",
+			"%" + competitor.search + "%"
+		]
+
+		connection.query(queryString, queryParameters, send_response);
+	}
+
+	function send_response(err, rows, args) {
+		if(err) {
+	      return res.status(500).send(err);
+	    } else {
+	      return res.send(rows);
+	    }
+	}
+
+	searchCompetitor();
+}
--- routes/router.js
+++ routes/router.js
@@ -3,5 +3,5 @@
 var express = require('express');
 var router = express.Router();
 
-var profileController = require("../controllers/sample-controller");
-router.get('/getSampleSession', profileController.getSampleSession);
+var userController = require("../controllers/user-controller");
+router.get('/searchCompetitor', userController.searchCompetitor);
