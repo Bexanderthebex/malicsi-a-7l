a577233aa7a9c95fdd4e00c1a59bd32c35ad08e5
Emy Jane Cabarles <enajyme1997@gmail.com>
2017-03-15 23:14:50 +0800

    fixed #69 edit-sponsor-details

--- controllers/sponsor-controller.js
+++ controllers/sponsor-controller.js
@@ -2,14 +2,15 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const connection = require('./../config/db-connection.js');
 
-exports.editSponsorDetails = (req, res) => {
-	var query = 'UPDATE sponsor_institution SET description = ? WHERE sponsor_id = ?';
-	connection.query(query, [req.body.description, req.body.sponsor_id], function(err, results, fields){
+exports.addSponsorToGame = (req, res) => {
+	var query = 'INSERT into sponsor_games VALUES(?, ?)';
+	connection.query(query, [req.body.sponsor_id, req.body.game_id], function(err, results, fields){
 		if(err) {
-			res.status(404).send("Unable to update sponsoring institution details!");
+			res.status(404).send("Unable to add sponsor.");
 			throw err;
+
 		}
-		else res.status(200).send("Successfully updated sponsoring institution details!");
+		else res.status(200).send("Successfully added sponsor to game!");
 	})
 }
 
