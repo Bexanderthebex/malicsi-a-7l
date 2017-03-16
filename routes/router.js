45bb803b4ab9f80919961b9d65e6e287895de30f
Emy Jane Cabarles <enajyme1997@gmail.com>
2017-03-16 22:25:50 +0800

    Revert "21 update game"

--- routes/router.js
+++ routes/router.js
@@ -8,29 +8,9 @@ var router = express.Router();
 
 var userController = require("../controllers/user-controller");
 var adminController = require('../controllers/admin-controller');
-var gameController = require("../controllers/game-controller");
-var sponsorController = require('../controllers/sponsor-controller')
-var sportController = require("../controllers/sport-controller");
-var matchController = require("../controllers/match-controller");
-
-function checkUser(req, res, next) {
-	console.log(req.session.user);
-	if (req.session.user !== undefined && (req.session.user.type === 'O' || req.session.user.type === 'A')) {
-		next();
-	} else {
-		res.status(403).send('Forbidden');
-	}
-}
 
 router.post('/login', userController.login);
 router.post('/organizer', adminController.createOrganizer);
-router.post('/register', userController.register)
-router.get('/game/:game_id', gameController.viewGameDetails);
-router.post('/game/addSponsor', sponsorController.addSponsorToGame);
-router.post('/createSport', sportController.createSport);
-router.post('/addMatch', checkUser, matchController.addMatch);
-router.get('/sport/:sportID', sportController.viewSportDetails);
-router.put('/game/sponsor/:sponsor_id', checkUser, sponsorController.editSponsorDetails);
-router.delete('/game/sponsor/:sponsor_id', checkUser, sponsorController.deleteSponsorFromGame);
+router.post('/register', userController.register);
 
 module.exports = router;
