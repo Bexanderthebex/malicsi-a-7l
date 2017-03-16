9c5b56175ee439fbf02dcc28eaa722c1fe32d5b5
aiMenguito <aimenguito1@up.edu.ph>
2017-03-16 23:39:28 +0900

    Update game-controller.js

--- controllers/game-controller.js
+++ controllers/game-controller.js
@@ -2,20 +2,13 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const connection = require('./../config/db-connection.js');
 
-exports.updateGame = function(req, res){
-	connection.query('UPDATE game SET ? WHERE id = ?', [req.body, req.body.gameId], function(err, rows){
-		if(err) res.status(500).send('Error in query.');
-		else {
-			selectGrade(req.body.id, function(rowUpdate){
-			if (err) res.status(500).send('Error in query');
-			if (rowUpdate == null){
-				res.status(552).send('Game ('+ req.body.id +') was not updated.');
-			} else {
-				res.status(200).send(rowUpdate);
-			}
-			});
+exports.deleteGame = function(req, res) {
+	connection.query('DELETE FROM grade WHERE id = ?', [req.body.id], function(err, row) {
+		if(err) res.status(500).send('Error in query');
+		if (row.affectedRows === 0) {
+			res.status(554).send('Game ('+req.body.id+') was not removed.');
+		} else {
+			res.status(200).send(row);
 		}
 	});
 }
-
-
