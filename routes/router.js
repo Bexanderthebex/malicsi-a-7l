2f9356fb25864d83737a58c8ee6c16ee6d0f239c
Anton Rufino <anton.vongola@gmail.com>
2017-03-17 18:13:46 +0800

    Create hashing middleware

--- routes/router.js
+++ routes/router.js
@@ -1,16 +1,29 @@
 'use strict'
 
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let crypto = require('crypto');
+let bcrypt = require('bcrypt');
 
-// var profileController = require("../controllers/sample-controller");
-// router.get('/getSampleSession', profileController.getSampleSession);
+let userController = require("../controllers/user-controller");
+let adminController = require('../controllers/admin-controller');
 
-var userController = require("../controllers/user-controller");
-var adminController = require('../controllers/admin-controller');
+function hashPassword(req, res, next) {
+    let hash = crypto.createHash('sha256');
+    hash.update(req.body.password);
+    bcrypt.hash(hash.digest('hex'), 10, (err, hash) => {
+        if (!err) {
+            req.body.password = hash;
+            next();
+        } else {
+            console.log(err);
+        }
+    });
+}
 
-router.post('/login', userController.login);
+router.post('/login', hashPassword, userController.login);
 router.post('/organizer', adminController.createOrganizer);
-router.post('/register', userController.register);
+router.post('/register', hashPassword, userController.register);
+router.post('/user/update', userController.update)
 
 module.exports = router;
