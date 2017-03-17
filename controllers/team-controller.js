98b4f2dbf2c1bb190e53037fd2840851914f7362
Frances-Lois <ffritualo@up.edu.ph>
2017-03-17 16:44:38 +0800

    modified team-controller

--- controllers/team-controller.js
+++ controllers/team-controller.js
@@ -2,9 +2,9 @@ var db = require(__dirname + './../config/db-connection');
 
 exports.createTeam = function(req, res){
     db.query("INSERT INTO team(id, sport_id, team_organization, team_sport, pending_participation) VALUES (?, ?, ?, ?, FALSE)",
-            [req.body.id,
+            [req.body.competitor_id,
             req.body.sport_id,
-            req.body.team_organization,
+            req.body.organization_id,
             req.body.team_sport],
             function(err){
                 if(err){
@@ -22,6 +22,21 @@ exports.deleteTeam = function(req, res){
     });
 };
 
-exports.teamMembershipRequest = function(){
-    
+exports.teamMembershipRequest = function(req, res){
+    db.query("INSERT INTO competitor_joins_team(id, team_id, is_member) VALUES(?,?,FALSE)",
+     [req.body.competitor_id, req.body.team_id],
+            function(err){
+                if(err){
+                    return err;
+                }
+            });
+}
+
+exports.acceptMembershipRequest = function(req, res){
+    db.query("UPDATE competitor_joins_team SET is_member = TRUE where id = ?", [req.body.competitor_id],
+    function(err){
+        if(err){
+            return err;
+        }
+    });
 }
