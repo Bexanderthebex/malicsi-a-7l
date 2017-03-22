cbcd7cfc8ceca4cf0e76838438a2476c6a92a90b
adriannoel <noeladrianzen@gmail.com>
2017-03-22 17:25:52 +0800

    closes #15

--- controllers/organizer-controller.js
+++ controllers/organizer-controller.js
@@ -42,3 +42,112 @@ exports.editOrganizer = (req,res) => {
 		}
 	});
 }
+
+exports.createGame = function(req,res,next){
+	connection.query("INSERT INTO game(name,description,location,start_date,end_date,orgz_id) VALUES (?, ?, ?, ?, ?, ?)"
+		, [	req.body.name,
+			req.body.description,
+			req.body.location,
+			req.body.start_date,
+			req.body.end_date,
+			req.body.orgz_id],
+			function(err,rows){
+		if(err) return next(err);
+		res.send(rows);
+	});
+};
+
+exports.updateGame = function(req,res,next){
+	
+	connection.query("UPDATE game SET name = ?,description = ?,location = ?,start_date = ?,end_date = ? WHERE game_id = ?"
+		, [	req.body.name,
+			req.body.description,
+			req.body.location,
+			req.body.start_date,
+			req.body.end_date,
+			req.body.game_id],
+			function(err,rows){
+		if(err) return next(err);
+		res.send(rows);
+	});
+};
+
+exports.editInfo = function(req,res,next){
+	
+	connection.query("UPDATE organizer SET name = ?,description = ? WHERE id = ?"
+		, [ req.body.name,
+			req.body.description,
+			req.body.id],
+			function(err,rows){
+		if(err) return next(err);
+		res.send(rows);
+	});
+};
+
+exports.findGames = function(req,res,next){
+
+	connection.query("SELECT * from game WHERE game.organizer_id = ?"
+		, [req.body.id],
+		function(err,rows){
+		if(!err){
+			res.status(200).send({'message' : 'Sucuessfully Retrieved Info',rows});
+			res.send(rows);
+		} else {
+			res.status(404).send({'message' : 'Information Not Found'})
+		}
+		});
+};
+
+exports.findSport = function(req,res,next){
+
+	connection.query("SELECT * from sport WHERE sport.game_id = ?"
+		, [req.body.game_id],
+		function(err,rows){
+		if(!err){
+			res.status(200).send({'message' : 'Sucuessfully Retrieved Info',rows});
+			res.send(rows);
+		} else {
+			res.status(404).send({'message' : 'Information Not Found'});
+		}
+		});
+};
+
+exports.findTeam = function(req,res,next){
+
+	connection.query("SELECT * from team WHERE team.sport_id = ?"
+		, [req.body.sport_id],
+		function(err,rows){
+		if(!err){
+			res.status(200).send({'message' : 'Sucuessfully Retrived Info',rows});
+		} else {
+			res.status(404).send({'message' : 'Information Not Found'});
+		}
+		});
+};
+
+
+exports.getRequest = function(req,res){
+	connection.query("SELECT * from team WHERE team_id = ?"
+	, [req,body.team_id],
+		function(err,rows){
+		if(!err){
+			res.status(200).send({'message' : 'Sucessfully Retrieved Info'})
+		} else {
+			res.status(404).send({'message' : 'Information Not Found'});
+		}
+	});
+};
+
+
+exports.acceptRequest = function(req,res){
+
+	connection.query("UPDATE team SET pending_participation = TRUE WHERE id =?"
+		, [req.body.team_id],
+		function(err,rows){
+			if(!err){
+				res.status(200).send({'message' : 'Sucuessfully Updated Request'});
+			} else {
+				res.status(404).send({'message' : 'Information Not Found'});
+			}
+		});
+};
