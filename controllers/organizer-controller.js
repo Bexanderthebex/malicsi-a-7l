37b4707c050942a80220f2a1e665c7ff9a6e3e6f
pvtan <pvtan@up.edu.ph>
2017-03-22 17:51:55 +0800

    Fixed #10

--- controllers/organizer-controller.js
+++ controllers/organizer-controller.js
@@ -5,7 +5,7 @@ const connection = require('./../config/db-connection.js');
 exports.searchOrganizer = (req, res) => {
 	query = 'SELECT * FROM organizer where name like ? or description like ?';
 
-	connection.query(query, ["%" + req.body.search + "%", "%" + req.body.search + "%"], function (err, rows) {
+	connection.query(query, ["%" + req.query.search + "%", "%" + req.query.search + "%"], function (err, rows) {
 		if(!err) {
 			if(rows.length == 1) {
 				res.status(200).send(rows[0]);
