c43d4ee88362ed7efa4d9d5eda5e33face67a1f3
yssel <blessysalvania@gmail.com>
2017-03-23 05:36:01 +0800

    Closes #39

---
+++ public/frontend_modules/js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.slider').slider();
+	// $('.slider').slider('pause');
+	// $('.slider').slider('start');
+	// $('.slider').slider('next');
+	// $('.slider').slider('prev');
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
