a97329bd5dccfc171042e646021161b645c5dc16
jlogsgln <jagosgolan@up.edu.ph>
2017-03-24 05:34:30 +0800

    Unit test

---
+++ user-test.js
@@ -0,0 +1,34 @@
+var request = require('supertest');
+var should = require('should-http');
+var url = 'http://localhost:3000';
+
+var user = { 	
+				'username':'John Louis Gosgolan', 
+				'password':'walakapagasa',
+				'email': 'jagosgolan@up.edu.ph',
+				'contact': '09167724643',
+				'type': 'user',
+				'is_active': true
+			};
+
+describe('MalICSi', function() {	//Describes the module
+	describe('/register', function() {	//Describes the specific feature
+		it('not respond to POST', function(done) {	//describes kung anung case yung tinetest
+			request(url)
+				.post('/register')
+				.end(function(err, res) {	//catches the http response
+					res.should.status(404);	//translated as "response should (have) status 404"
+					done();
+				});
+		});
+		it('respond to POST (Successfully registered)', function(done){
+			request(url)
+				.post('/register')
+				.send({json: user}) 
+				.end(function(err,res){
+					res.should.status(200);
+					done();
+				});
+		});
+	});
+});
