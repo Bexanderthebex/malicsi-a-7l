ae52f7d7f1e83461fe2b44a29c36b67fc417ebd5
Bexanderthebex <melbex11@gmail.com>
2017-03-24 01:26:35 +0800

    fixed #29,#30,#31

--- controllers/sport-controller.js
+++ controllers/sport-controller.js
@@ -1,7 +1,22 @@
-var db = require(__dirname + './../config/db-connection');
+var connection = require(__dirname + './../config/db-connection');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 
+<<<<<<< HEAD
+exports.editSport = function(req, res, next){
+	connection.query("UPDATE sport SET time_start = ?, time_end = ?, date = ?, scoring_system = ? WHERE sport_id = ?"
+			, [req.body.time_start,
+			   req.body.time_end,
+			   req.body.date,
+			   req.body.scoring_system,
+			   req.body.sport_id],
+			   function(err, rows){
+			   if(!err)
+			        res.status(200).send(rows); 
+			   else
+			        res.status(404).send("edit unsuccessful. error occured");
+		});
+=======
 exports.createSport = (req, res) => {
 	const newSport = {
 		time_start: req.body.timeStart,
@@ -30,40 +45,40 @@ exports.deleteSport = function(req, res, next){
 		function(err,rows){
 		if(err) return next(err);
 	});
+>>>>>>> c73899646b9e84d3b599567fab4ea46aa9cf0b18
 }
 
 exports.addWinnerSport = function(req, res, next){
-	db.query("UPDATE sport SET winner = ? WHERE sport_id = ?"
+	connection.query("UPDATE sport SET winner = ? WHERE sport_id = ?"
 			,[req.body.winner,
 			  req.body.sport_id],
 			function(err, rows){
-			if(err) return next(err);
-			res.send(rows);
+			if(!err) 
+				res.status(200).send(rows);
+			else if(rows.length = undefined)
+				res.status(204).send(req.body.sport_id + " already updated");
+			else
+				res.status(404).send("update unsuccessful. error occured");
+
+			
 	});
 }
 
-exports.editSport = function(req, res, next){
-	db.query("UPDATE sport SET time_start = ?, time_end = ?, date =?, scoring_system = ? WHERE sport_id = ?"
-		 , [req.body.time_start,
-		    req.body.time_end,
-		    req.body.date,
-		    req.body.scoring_system,
-		    req.body.sport_id],
-		    function(err, rows){
-		    if(err) return next(err);
-		    res.send(rows);
-	});
-
-exports.createSportSchedule = (req, res) => {
-	//perform addMatch() (if matches are not yet created)
-	//retrieve from db the matches for the specific sport
-	//sort the results according to time 
-	var query = 'SELECT * FROM sport_match WHERE sport_id = ?';
-	connection.query(query, [req.body.sport_id], function(err, results, fields){
-		if(err) {
-			res.status(404).send("Unable to create schedule.");
-			throw err;
+<<<<<<< HEAD
+exports.deleteSport = function(req, res, next){
+	connection.query("DELETE FROM sport WHERE sport_id = ?"
+		,[req.body.sport_id],
+		function(err,rows){
+		if(!err) {
+			res.status(200).send("successfully deleted " + req.body.sport_id);
 		}
-		else res.status(200).send(rows);
+		else if(rows.length == undefined ){ 
+			res.status(204).send(req.body.sport_id + " not found!");
+		}
+		else{
+			res.status(404).send("delete unsuccessful. error occured");
+		}
 	});
 }
+=======
+>>>>>>> c73899646b9e84d3b599567fab4ea46aa9cf0b18
