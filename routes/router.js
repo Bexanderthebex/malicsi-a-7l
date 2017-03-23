865decddacd20f10541fabdb0c9d9bf9db00d500
claizelcoubeili <clcepe@up.edu.ph>
2017-03-23 17:53:48 +0800

    fixed #50 #9

--- routes/router.js
+++ routes/router.js
@@ -21,17 +21,17 @@ router.post('/register', userController.register);
 router.get('/getUserInfo',userController.getUserInfo);
 
 //competitor routers
-router.get('/searchCompetitor', competitorController.searchCompetitor);
-router.put('/editCompetitor', competitorController.editCompetitor);
+router.get('/competitor/searchCompetitor', competitorController.searchCompetitor);
+router.put('/competitor/editCompetitor', competitorController.editCompetitor);
 
 //organizer routers
-router.get('/searchOrganizer', organizerController.searchOrganizer);
-router.put('/editOrganizer', organizerController.editOrganizer);
+router.get('/organizer/searchOrganizer', organizerController.searchOrganizer);
+router.put('/organizer/editOrganizer', organizerController.editOrganizer);
 
 //team routers
-router.post('/createTeam',teamController.createTeam);
-router.post('/deleteTeam',teamController.deleteTeam);
-router.post('/teamMembershipRequest',teamController.teamMembershipRequest);
-router.post('/acceptMembershipRequest',teamController.acceptMembershipRequest);
+router.post('/team/createTeam',teamController.createTeam);
+router.post('/team/deleteTeam',teamController.deleteTeam);
+router.post('/team/teamMembershipRequest',teamController.teamMembershipRequest);
+router.post('/team/acceptMembershipRequest',teamController.acceptMembershipRequest);
 
 module.exports = router;
