7da50e98935a31dd910f3d6cb55609cf07c56710
Anton Rufino <anton.vongola@gmail.com>
2017-03-24 16:52:51 +0800

    Fix merge

--- test/user-test.js
+++ test/user-test.js
@@ -32,7 +32,7 @@ describe('MalICSi', function() {	//Describes the module
 			request(url)
 				.post('/register')
 				.send({
-						'username':'jlogsgln',
+						'username':'jpelaez',
 						'password':'hehehe',
 						'email': 'example@up.edu.ph',
 						'contact': '0912345678',
@@ -40,7 +40,7 @@ describe('MalICSi', function() {	//Describes the module
 						'is_active': 1
 					})
 				.end(function(err,res){
-					res.should.status(200);
+					res.should.status(404);
 					done();
 				});
 		});
@@ -53,6 +53,46 @@ describe('MalICSi', function() {	//Describes the module
 					})
 				.end(function(err,res){
 					res.should.status(404);
+					done();
+				});
+		});
+	});
+
+	describe('/login', function() {	//Describes the specific feature
+		it('not respond to GET', function(done) {	//describes kung anung case yung tinetest
+			request(url)
+				.get('/login')
+				.end(function(err, res) {	//catches the http response
+					res.should.status(404);	//translated as "response should (have) status 404"
+					done();
+				});
+		});
+		it('respond to POST (Successfully logged in)', function(done){
+			request(url)
+				.post('/login')
+				.send({ 'username':'jpelaez', 'password':'hihihi'})
+				.end(function(err,res){
+					res.should.status(200);
+					done();
+				});
+		});
+		it('respond to POST (Incorrect password)', function(done){
+			request(url)
+				.post('/login')
+				.send({ 'username':'jpelaez', 'password': 'kek' })
+				.end(function(err,res) {
+					res.should.status(404);
+					res.body.should.have.property('message').eql('Incorrect credentials');
+					done();
+				});
+		});
+		it('respond to POST (User does not exist!)', function(done) {
+			request(url)
+				.post('/login')
+				.send({ 'username':'mcdo' })
+				.end(function(err,res){
+					res.should.status(404);
+					res.body.should.have.property('message').eql('Incorrect credentials');
 					done();
 				});
 		});
