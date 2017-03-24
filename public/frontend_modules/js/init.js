ab5d2ebc568065c783ae5d9cbb055e18f057f59e
jjdcabasolo <abasolojohnjourish@gmail.com>
2017-03-24 09:56:02 +0800

    closes #149

--- public/frontend_modules/js/init.js
+++ public/frontend_modules/js/init.js
@@ -1,16 +1,16 @@
 (function($){
-  $(function(){
+	$(function(){
 
-    $('.button-collapse').sideNav();
-    $('.slider').slider();
+		$('.button-collapse').sideNav();
+		$('.slider').slider();
 		$(document).ready(function() {
-		    $('.modal').modal();
+			$('.modal').modal();
 		});
-    
-    $('.datepicker').pickadate({
-	    selectMonths: true, // Creates a dropdown to control month
-	    selectYears: 15 // Creates a dropdown of 15 years to control year
-	  });    
+		
+		$('.datepicker').pickadate({
+			selectMonths: true, // Creates a dropdown to control month
+			selectYears: 15 // Creates a dropdown of 15 years to control year
+		});		
 
-  }); // end of document ready
+	}); // end of document ready
 })(jQuery); // end of jQuery name space
