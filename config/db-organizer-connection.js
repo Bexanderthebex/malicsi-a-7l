4086a4fc897206fde9c167f655364c1fbb203765
Ralph Anthony A. Reyes <raareyes11@up.edu.ph>
2017-03-27 22:12:15 +0800

    created new database users

---
+++ config/db-organizer-connection.js
@@ -0,0 +1,21 @@
+'use strict'
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host : 'localhost',
+  user : 'organizer',
+  password : 'password3',
+  db : 'malicsi'
+});
+
+connection.connect((err) => {
+    if (!err) {
+        console.log("Database connected!");
+    } else {
+        console.log("Error in database connection!");
+    }
+});
+
+connection.query('USE malicsi');
+module.exports = connection;
+
