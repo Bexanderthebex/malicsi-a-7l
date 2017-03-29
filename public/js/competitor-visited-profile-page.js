f2a9929b1b23c9930f49e79b7f9806b87bfc445f
clarixeon <carolinebriontolentino@gmail.com>
2017-03-29 15:45:10 +0800

    visited competitor profile page ui

--- public/js/competitor-visited-profile-page.js
+++ public/js/competitor-visited-profile-page.js
@@ -1,9 +1,36 @@
 $(document).ready(function(){
-	$('.modal-trigger').on('click', function() {
-        $('#edit-profile-modal').openModal();
-    });
+    $('body').css('overflow', '');
+    
+    $('ul.tabs').tabs();
+
+    // $('.slide').slick({
+    //     infinite: true,
+    //     slidesToShow: 2,
+    //     slidesToScroll: 2,
+    //     autoplay: true,
+    //     rows: 2
+    // });
+
+    $('.tooltipped').tooltip();
+    
     $('.modal').modal();
-	$('.game-item').on({
+
+    $('#visited-competitor-profile-edit').on('click', function() {
+        $('#visited-competitor-profile-edit-modal').openModal();
+    });
+
+    $('#visited-competitor-profile-accept').on('click', function() {
+        $('#visited-competitor-profile-accept-modal').openModal();
+    });
+
+    $('#visited-competitor-profile-decline').on('click', function() {
+        $('#visited-competitor-profile-decline-modal').openModal();
+    });
+
+    $('.datepicker').pickadate({
+        format: 'yyyy-mm-dd'
+    });
+    $('.game-item').on({
         mouseover: function() {
             $(this).find('.game-desc').fadeIn(200);
         },
@@ -11,14 +38,47 @@ $(document).ready(function(){
             $(this).find('.game-desc').stop().fadeOut(200);
         },
     });
-	$('.game-desc').css('height', $(".visited-game-img").height());
-    $('.game-desc').css('width', $(".visited-game-img").width());
-    $('.datepicker').pickdate({
-    	selectMonths: true,
-    	selectYears: 20
-    })
-	$('select').material_select();
-	for (i = new Date().getFullYear(); i>1900; i--){
-		$('#yearpicker').append($('<option />').val(i).html(i));
-	}
+    $('.game-desc').css('height', $(".visited-competitor-game-img").height());
+    $('.game-desc').css('width', $(".visited-competitor-game-img").width());
+
+    /*************************************************************
+        var chip = {
+            tag: 'chip content',
+            image: '', //optional
+            id: 1, //optional
+        };
+
+        <div class="chips chips-autocomplete"></div>
+        
+        *************************************************
+
+        var my_data = {
+                "0":"Apple",
+                "1":"Microsoft",
+                "2":"Google"
+        }
+
+        var myConvertedData = {};
+
+        $.each(my_data, function(index, value) {
+            myConvertedData[value] = null;
+        });
+
+        $('.chips-autocomplete').material_chip({
+            autocompleteData: myConvertedData
+        });
+    *************************************************************/
+
+    $('.chips').material_chip();
+    $('.chips-autocomplete').material_chip({
+        autocompleteOptions: {
+            data: {
+                'Jane Doe': null,
+                'Jean Doe': null,
+                'Joan Doe': null
+            },
+            limit: Infinity,
+            minLength: 1
+        }
+    });
 });
