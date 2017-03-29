d7c53b51f83cbab9af779b8e90e2b1c6b5ca0d2f
clarixeon <carolinebriontolentino@gmail.com>
2017-03-29 14:09:20 +0800

    competitor profile components

--- public/js/competitor-visited-profile-page.js
+++ public/js/competitor-visited-profile-page.js
@@ -11,11 +11,14 @@ $(document).ready(function(){
             $(this).find('.game-desc').stop().fadeOut(200);
         },
     });
-	$('.game-desc').css('height', $(".competitor-game-img").height());
-    $('.game-desc').css('width', $(".competitor-game-img").width());
+	$('.game-desc').css('height', $(".visited-game-img").height());
+    $('.game-desc').css('width', $(".visited-game-img").width());
     $('.datepicker').pickdate({
     	selectMonths: true,
     	selectYears: 20
     })
-	$('.chips').material_chip();
+	$('select').material_select();
+	for (i = new Date().getFullYear(); i>1900; i--){
+		$('#yearpicker').append($('<option />').val(i).html(i));
+	}
 });
