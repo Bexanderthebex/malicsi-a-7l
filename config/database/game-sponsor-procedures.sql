b189112dbad41b4a78372a633301a8822eb7ec7a
Emy Jane Cabarles <enajyme1997@gmail.com>
2017-03-30 22:46:33 +0800

    Update and rename sponsor-procedures.sql to game-sponsor-procedures.sql

---
+++ config/database/game-sponsor-procedures.sql
@@ -0,0 +1,29 @@
+DELIMITER //
+CREATE PROCEDURE add_sponsor_to_game(IN game_id INT,  IN sponsor_id INT)
+BEGIN
+	INSERT INTO sponsor_games VALUES (sponsor_id, game_id);
+END;
+//
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE edit_sponsor_details (IN descr TEXT, IN spon_id INT)
+BEGIN
+	UPDATE sponsor_institution SET description = descr WHERE sponsor_id = spon_id;
+END//
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE delete_sponsor_from_game (IN spon_id INT, IN g_id INT)
+BEGIN
+	DELETE FROM sponsor_institution WHERE sponsor_id = spon_id AND game_id = g_id;
+END//
+DELIMITER ;
+
+GRANT EXECUTE ON PROCEDURE add_sponsor_to_game TO ORGANIZER;
+GRANT EXECUTE ON PROCEDURE add_sponsor_to_game TO ADMINISTRATOR;
+GRANT EXECUTE ON PROCEDURE edit_sponsor_details TO ORGANIZER;
+GRANT EXECUTE ON PROCEDURE edit_sponsor_details TO ADMINISTRATOR;
+grant execute on procedure delete_sponsor_from_game to organizer;
+grant execute on procedure delete_sponsor_from_game to administrator;
