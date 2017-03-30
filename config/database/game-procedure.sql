4a2f6c41aeaaf49fec3dfdc75f90daad53adfa24
Emy Jane Cabarles <enajyme1997@gmail.com>
2017-03-30 22:35:23 +0800

    Update and rename game-procedures.sql to game-procedure.sql

--- config/database/game-procedure.sql
+++ config/database/game-procedure.sql
@@ -1,11 +1,11 @@
 delimiter //
-create procedure viewGameDetails(in game_id int)
+create procedure view_game_details(in game_id int)
 BEGIN
 	select game.name, start_date,end_date, location, game.description, organizer.name as organizer_name , organizer.description as organizer_description, datediff(end_date, start_date) as game_duration from game,organizer where game.organizer_id = organizer.id and game.game_id = game_id;
 END;
 //
 delimiter ;
-grant execute on procedure viewGameDetails to organizer;
-grant execute on procedure viewGameDetails to competitor;
-grant execute on procedure viewGameDetails to administrator;
-grant execute on procedure viewGameDetails to guest;
+grant execute on procedure view_game_details to organizer;
+grant execute on procedure view_game_details to competitor;
+grant execute on procedure view_game_details to administrator;
+grant execute on procedure view_game_details to guest;
