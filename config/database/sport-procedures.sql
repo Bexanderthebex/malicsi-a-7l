645d3b6ec89a3874f8cc0884cc8a8203a54418b8
Emy Jane Cabarles <enajyme1997@gmail.com>
2017-03-31 14:29:29 +0800

    Update sport-procedures.sql

--- config/database/sport-procedures.sql
+++ config/database/sport-procedures.sql
@@ -1,6 +1,6 @@
 
 delimiter //
-	create procedure editSport(in sportName varchar(50), in mechanicsInput text, in timeStart time, in timeEnd time, in startDate date, in endDate date, in sportDate date, in scoringSystem varchar(50), in sportId int(11))
+	create procedure edit_sport(in sportName varchar(50), in mechanicsInput text, in timeStart time, in timeEnd time, in startDate date, in endDate date, in sportDate date, in scoringSystem varchar(50), in sportId int(11))
 	BEGIN
 		UPDATE sport SET sport_name = sportName, mechanics = mechanicsInput, time_start = timeStart, time_end = timeEnd, start_end = startDate, end_date = endDate, sport_date = sportDate, scoring_system = scoringSystem WHERE sport_id = sportId;
 	END;
@@ -8,7 +8,7 @@ delimiter //
 delimiter ;
 
 delimiter //
-	create procedure addWinnerSport(in winnerInput int(11), in sportId int(11))
+	create procedure add_winner_sport(in winnerInput int(11), in sportId int(11))
 	BEGIN
 		UPDATE sport SET winner = winnerInput WHERE sport_id = sport_id;
 	END;
@@ -16,7 +16,7 @@ delimiter //
 delimiter ;
 
 delimiter //
-	create procedure deleteSport(in sportId int(11))
+	create procedure delete_sport(in sportId int(11))
 	BEGIN
 		DELETE FROM sport WHERE sport_id = sportId;
 	END;
@@ -24,6 +24,9 @@ delimiter //
 delimiter ;
 
 
-GRANT execute ON procedure deleteSport to organizer;
-GRANT execute ON procedure addWinnerSport to organizer;
-GRANT execute ON procedure editSport to organizer;
+GRANT execute ON procedure delete_sport to organizer;
+GRANT execute ON procedure add_winner_sport to organizer;
+GRANT execute ON procedure edit_sport to organizer;
+GRANT execute ON procedure delete_sport to administrator;
+GRANT execute ON procedure add_winner_sport to administrator;
+GRANT execute ON procedure edit_sport to administrator;
