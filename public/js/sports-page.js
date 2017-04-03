db495dd1257da9cbacc502f11b6f9e8bf2715406
yssel <blessysalvania@gmail.com>
2017-04-03 22:38:57 +0800

    Added Matches Section in sports page

---
+++ public/js/sports-page.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.tooltipped').tooltip();
+    
+    $('.modal').modal();
+
+    $('#sports-ranking-view-all').on('click', function() {
+        $('#rankings-view-all-modal').openModal();
+    });
+
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+});
