8ba29a423f06e0848a05fef6d38bd14e48d9b02b
yssel <blessysalvania@gmail.com>
2017-04-04 00:01:51 +0800

    Added edit match modal and Sponsor Footer

--- public/js/sports-page.js
+++ public/js/sports-page.js
@@ -7,6 +7,10 @@ $(document).ready(function(){
         $('#rankings-view-all-modal').openModal();
     });
 
+    $('.sports-match').on('click', function() {
+        $('#match-edit-modal').openModal();
+    });
+
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15 // Creates a dropdown of 15 years to control year
