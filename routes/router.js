4a05df819f85ede62afbe10dc0da59752b0decad
yssel <blessysalvania@gmail.com>
2017-04-06 12:18:35 +0800

    Commented out organizer processRequest in controllers

--- routes/router.js
+++ routes/router.js
@@ -78,7 +78,7 @@ router.get('/organizer/findSport',organizerController.findSport);
 router.get('/organizer/findTeam',organizerController.findTeam);
 router.put('/organizer/editOrganizer', organizerController.editOrganizer);
 router.get('/organizer/getRequest', organizerController.getRequest);
-router.put('/organizer/processRequest', organizerController.processRequest);
+// router.put('/organizer/processRequest', organizerController.processRequest);
 router.get('/organizer/getPendingParticipation', organizerController.getPendingParticipation);
 router.get('/organizer/findGames',organizerController.findGames);
 router.get('/organizer/getOrganizer',organizerController.getOrganizer);
