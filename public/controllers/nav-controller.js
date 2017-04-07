23d3e2cd3776a933e5038b06be32cc64150feb5f
claizelcoubeili <clcepe@up.edu.ph>
2017-04-07 13:02:16 +0800

    fixed ng-hide

--- public/controllers/nav-controller.js
+++ public/controllers/nav-controller.js
@@ -6,19 +6,24 @@
 
     function NavCtrl($scope, $http) {
         $scope.iconUrl = "";
+        $scope.user = {};
+        $scope.hide = true;
 
         $http.get('/user')
         .then(function(result){
             if (result.data === "") {
                 console.log("kek", result);
                 $scope.iconUrl = '#/login';
+                $scope.user = {};
             } else {
+                $scope.user = result.data;
+                $scope.hide = false;
                 console.log(result.data);
-                if (result.data[0].type === "C") {
+                if (result.data.type === "C") {
                     $scope.iconUrl = '#/competitor/profile';
-                } else if(result.data[0].type == "O") {
+                } else if(result.data.type ==="O") {
                     $scope.iconUrl = '#/organizer/profile';
-                } else if(result.data[0].type == "A") {
+                } else if(result.data[0].type === "A") {
                     $scope.iconUrl = '#/adminpanel'
                 }
             }
