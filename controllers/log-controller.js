fe5ffb9ba0c9bbb9edd3b945e071a7260974d274
claizelcoubeili <clcepe@up.edu.ph>
2017-04-12 07:12:18 +0800

    Update log-controller.js

--- controllers/log-controller.js
+++ controllers/log-controller.js
@@ -17,11 +17,16 @@ exports.viewAllLogs = (req, res) => {
 exports.viewLogsByDate = (req, res) => {
 	query = 'SELECT * FROM log WHERE date_created BETWEEN ? AND ?';
 
-	connection.userType('A').query(query, [req.body.startDate, req.body.endDate], function (err, rows) {
-		if(!err) {
-			return res.status(200).send(rows);
-		} else {
-			return res.status(500).send({'message' : 'Internal Server Error'});
+	connection.userType('A').query(query,
+		[
+			req.body.startDate,
+			req.body.endDate
+		], (err, rows) => {
+			if(!err) {
+				return res.status(200).send(rows);
+			} else {
+				return res.status(500).send({'message' : 'Internal Server Error'});
+			}
 		}
-	});
+	);
 }
