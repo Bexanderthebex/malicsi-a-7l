c91e49f6aafebbcdb7e44b0b7228ee5f0b9a5c37
Kenichii Ana <ana.kenichii@gmail.com>
2017-04-19 08:51:39 +0800

    Changed organization page's modal header's color

--- config/db-competitor-connection.js
+++ config/db-competitor-connection.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host : 'localhost',
-  user : 'competitor',
-  password : 'password2',
+  user : 'root',
+  password : '31415926535',
   db : 'malicsi',
   dateStrings: true
 });
