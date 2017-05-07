9be2fd0b6e67adf535d81a30d06349e6b9bb336d
yssel <blessysalvania@gmail.com>
2017-05-07 18:15:03 +0800

    Responsive Login page

--- public/js/login-page.js
+++ public/js/login-page.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
 		$("#title-left").css("display", "none");
 	});
 
+	$("#signup-small").on('click', function(){
+		$("#login-login").css("display", "none");
+		$("#login-signup").css("display", "inline");
+	});
+
 	$("#backtologin").on('click', function(){
 		$("#title-right").css("display", "none");
 		$("#content-right").css("display", "");
@@ -13,10 +18,31 @@ $(document).ready(function(){
 		$("#title-left").css("display", "");
 	});
 
+	$("#backtologin-small").on('click', function(){
+		$("#login-login").css("display", "inline");
+		$("#login-signup").css("display", "none");
+	});
+
 	$('.datepicker').pickadate({
 		format: 'yyyy-mm-dd',
 		selectYears: 75,
 		selectMonths: true
+	});
+
+	if ($(window).width() > 600){
+	    $("#login-cards-small").css("display", "none");
+	}else{
+	    $("#login-cards-small").css("display", "inline");
+	}
+
+	$(window).resize(function() {
+		if ($(window).width() > 600){
+			console.log("bakit");
+		    $("#login-cards-small").css("display", "none");
+		}else{
+			console.log("please lord");
+		    $("#login-cards-small").css("display", "inline");
+		}
 	});
 
 });
